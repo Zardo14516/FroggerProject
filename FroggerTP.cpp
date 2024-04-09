d15478b3c501373facc5e5f157194bb50b6b68f2
Zardo14516 <133469964+Zardo14516@users.noreply.github.com>
2024-04-09 20:58:00 -0300

    Update FroggerTP.cpp
    Corrección del TP agregando herencia al código donde Frog y Car son clases separadas y luego FroggerGame las hereda.

--- FroggerTP.cpp
+++ FroggerTP.cpp
@@ -1,11 +1,10 @@
-#include<iostream>
-#include<windows.h>
-#include<ctime>
-#include<conio.h>
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <conio.h>
 
 using namespace std;
 
-//introducir tamaños y caracteres
 const int SCREEN_WIDTH = 40;
 const int SCREEN_HEIGHT = 10;
 const char FROG = 'F';
@@ -13,119 +12,135 @@ const char EMPTY = '.';
 const char CAR = 'C';
 const char BORDER = '#';
 
-//Creacion de clase Frogger conteniendo el frog, los autos y sus posiciones y velocidades
-class FroggerGame{
-private:
-	int frogPosicionX, frogPosicionY;
-	int car1Posicion, car2Posicion, car3Posicion;
-	int car1Velocidad, car2Velocidad, car3Velocidad;
+class Frog {
+protected:
+	int x, y;
 	
 public:
-	FroggerGame() : frogPosicionX(SCREEN_WIDTH / 2), frogPosicionY(SCREEN_HEIGHT - 1),
-		        car1Posicion(0), car2Posicion(SCREEN_WIDTH / 3), car3Posicion(2 * SCREEN_WIDTH / 3),
-		        car1Velocidad(rand() % 3 + 1), car2Velocidad(rand() % 3 + 1), car3Velocidad(rand() % 3 + 1) {
-		srand(static_cast<unsigned>(time(0)));  //Semilla para generacion aleatoria de numeros
+	Frog(int initialX, int initialY) : x(initialX), y(initialY) {}
+	
+	int getX() const {
+		return x;
 	}
 	
-	void drawScreen(){
-		system("cls"); //system cls para limpiar la pantalla
-		
-		//Dibujar borde
-		for (int i = 0; i < SCREEN_WIDTH + 2; ++i){
-			cout<<BORDER; 
-		}
-		cout<<endl;
-		
-		for (int i = 0; i < SCREEN_HEIGHT; ++i){
-			cout<<BORDER;
-			for (int j = 0; j < SCREEN_WIDTH; ++j) {
-				if (i == frogPosicionY && j == frogPosicionX) {
-					cout << FROG;
-				} else if (i == SCREEN_HEIGHT - 2 && (j == car1Posicion || j == car2Posicion || j == car3Posicion)) {
-					cout << CAR;
-				} else {
-					cout << EMPTY;
-				}
-			}
-			cout << BORDER << endl;
-		}
-		for (int i = 0; i < SCREEN_WIDTH + 2; ++i) {
-			cout << BORDER;
-		}
-		cout << endl;
-		
-	}	
-		
+	int getY() const {
+		return y;
+	}
+	
 	void moveFrog(char direction) {
-		switch (direction){
+		switch (direction) {
 		case 'w':
-			if (frogPosicionY > 1){
-				frogPosicionY--;
-			}
+			if (y > 1) y--;
 			break;
 		case 's':
-			if (frogPosicionY < SCREEN_HEIGHT - 2){
-				frogPosicionY++;
-			}
+			if (y < SCREEN_HEIGHT - 2) y++;
 			break;
 		case 'a':
-			if (frogPosicionX > 1){
-				frogPosicionX--;
-			}
+			if (x > 1) x--;
 			break;
 		case 'd':
-			if (frogPosicionX < SCREEN_WIDTH - 2){
-				frogPosicionX++;
-			}
+			if (x < SCREEN_WIDTH - 2) x++;
 			break;
-		}
-	}	
-	
-	void respawnFrog(){
-		frogPosicionX = SCREEN_WIDTH / 2;
-		frogPosicionY = SCREEN_HEIGHT - 1;
-	}
-	
-	void moverCars() {
-		car1Posicion = (car1Posicion + car1Velocidad) % SCREEN_WIDTH;
-		car2Posicion = (car2Posicion + car2Velocidad) % SCREEN_WIDTH;
-		car3Posicion = (car3Posicion + car3Velocidad) % SCREEN_WIDTH;
-	}
-		
-	bool checkCollision(){
-		return (frogPosicionX == car1Posicion && frogPosicionY == SCREEN_HEIGHT - 2) ||
-			   (frogPosicionX == car2Posicion && frogPosicionY == SCREEN_HEIGHT - 2) ||
-			   (frogPosicionX == car3Posicion && frogPosicionY == SCREEN_HEIGHT - 2);
-	}
-		
-		
-	void play(){
-		while (true) {
-			drawScreen();
-			
-			if (_kbhit()){
-			char key = _getch();
-			moveFrog(key);
-			}
-			moverCars();
-			
-			if (checkCollision()){
-				cout <<"Pisaron la rana!" << endl;
-				Sleep(1000); 
-				respawnFrog();
-			}
-			
-			Sleep(100); // Dormir por 100 millisegundos
 		}
 	}
 };
 
+class Car {
+protected:
+	int position;
+	int speed;
+	
+public:
+	Car(int initialPosition, int initialSpeed) : position(initialPosition), speed(initialSpeed) {}
+	
+	void move() {
+		position = (position + speed) % SCREEN_WIDTH;
+	}
+	
+	int getPosition() const {
+		return position;
+	}
+};
+
+class FroggerGame : public Frog, public Car {
+private:
+	int car2Position, car3Position;
+	int car2Speed, car3Speed;
+	
+public:
+	FroggerGame() : Frog(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 1), Car(0, rand() % 3 + 1),
+		car2Position(SCREEN_WIDTH / 3), car3Position(2 * SCREEN_WIDTH / 3),
+		car2Speed(rand() % 3 + 1), car3Speed(rand() % 3 + 1) {
+		srand(static_cast<unsigned>(time(0)));
+	}
+	
+	void drawScreen() {
+		system("cls");
+		
+		// Draw border
+		for (int i = 0; i < SCREEN_WIDTH + 2; ++i)
+			cout << BORDER;
+		cout << endl;
+		
+		for (int i = 0; i < SCREEN_HEIGHT; ++i) {
+			cout << BORDER;
+			for (int j = 0; j < SCREEN_WIDTH; ++j) {
+				if (i == getY() && j == getX())
+					cout << FROG;
+				else if (i == SCREEN_HEIGHT - 2 && (j == getPosition() || j == car2Position || j == car3Position))
+					cout << CAR;
+				else
+					cout << EMPTY;
+			}
+			cout << BORDER << endl;
+		}
+		
+		for (int i = 0; i < SCREEN_WIDTH + 2; ++i)
+			cout << BORDER;
+		cout << endl;
+	}
+	
+	bool checkCollision() {
+		return (getX() == getPosition() && getY() == SCREEN_HEIGHT - 2) ||
+			(getX() == car2Position && getY() == SCREEN_HEIGHT - 2) ||
+			(getX() == car3Position && getY() == SCREEN_HEIGHT - 2);
+	}
+	
+	void respawnFrog() {
+		x = SCREEN_WIDTH / 2;
+		y = SCREEN_HEIGHT - 1;
+	}
+	
+	void moverCars() {
+		
+		car2Position = (car2Position + car2Speed) % SCREEN_WIDTH;
+		car3Position = (car3Position + car3Speed) % SCREEN_WIDTH;
+	}
+	
+	void play() {
+		while (true) {
+			drawScreen();
+			
+			if (_kbhit()) {
+				char key = _getch();
+				moveFrog(key);
+			}
+			moverCars();
+			
+			if (checkCollision()) {
+				cout << "Pisaron la rana!" << endl;
+				Sleep(1000);
+				respawnFrog();
+			}
+			
+			Sleep(100);
+		}
+	}
+};
 
 int main(int argc, char *argv[]) {
-	
 	FroggerGame game;
 	game.play();
 	
 	return 0;
 }
-
